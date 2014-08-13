94f8bc5e7f6838855acb14855f9421dbe106196a
alinddicu <alinddicu@yahoo.com>
2014-08-13 23:05:35 +0200

    Started Regexp parsing for 1st degree equation formula

---
+++ FirstDegreeGraphicEquationSolver/FirstDegreeGraphicEquationSolver/Tools/FirstDegreeEquationPhraseParser.cs
@@ -0,0 +1,29 @@
+﻿namespace FirstDegreeGraphicEquationSolver.Tools
+{
+    using System.Globalization;
+    using System.Text.RegularExpressions;
+
+    public class FirstDegreeEquationPhraseParser
+    {
+        // (+/-)ax(+/-)b
+        private static readonly Regex VerifyPhraseRegex = new Regex(@"^(\+?|\-?)(\d*)(\.?)(\d*)(x)(\+?|\-?)(\d*)(\.?)(\d*)$");
+        private static readonly Regex ExtractRegex = new Regex(@"^(?<a>\+?|\-?\d*\.d*)x(?<b>\+?|\-?\d*\.d*)$");
+        private static readonly CultureInfo FormatProvider = new CultureInfo("en-US");
+
+        public bool CanParse(string phrase)
+        {
+            return VerifyPhraseRegex.IsMatch(phrase);
+        }
+
+        public double[] Extract(string phrase)
+        {
+            var match = ExtractRegex.Match(phrase);
+            var aMatchValue = match.Groups["a"].Value;
+            var bMatchValue = match.Groups["b"].Value;
+            var a = double.Parse(string.IsNullOrEmpty(aMatchValue) ? "1" : aMatchValue, FormatProvider);
+            var b = double.Parse(string.IsNullOrEmpty(bMatchValue) ? "0" : bMatchValue, FormatProvider);
+
+            return new[] { a, b };
+        }
+    }
+}
---
+++ FirstDegreeGraphicEquationSolver/Test/FirstDegreeEquationPhraseParserTest.cs
@@ -0,0 +1,96 @@
+﻿namespace Test
+{
+    using FirstDegreeGraphicEquationSolver.Tools;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using NFluent;
+
+    [TestClass]
+    public class FirstDegreeEquationPhraseParserTest
+    {
+        private FirstDegreeEquationPhraseParser _parser = new FirstDegreeEquationPhraseParser();
+
+        [TestMethod]
+        public void TestX()
+        {
+            Check.That(_parser.CanParse("x")).Equals(true);
+            Check.That(_parser.Extract("x")).ContainsExactly(new[] { 1.0, 0.0 });
+        }
+
+        [TestMethod]
+        public void Test2X()
+        {
+            Check.That(_parser.CanParse("2x")).Equals(true);
+            Check.That(_parser.Extract("2x")).ContainsExactly(new[] { 2.0, 0.0 });
+        }
+
+        [TestMethod]
+        public void Test2Point1X()
+        {
+            Check.That(_parser.CanParse("2.1x")).Equals(true);
+            Check.That(_parser.Extract("2.1x")).ContainsExactly(new[] { 2.1, 0.0 });
+        }
+
+        [TestMethod]
+        public void Test2Point1XPlus2Point1()
+        {
+            Check.That(_parser.CanParse("2.1x+2.1")).Equals(true);
+            Check.That(_parser.Extract("2.1x+2.1")).ContainsExactly(new[] { 2.1, 2.1 });
+        }
+
+        [TestMethod]
+        public void TestMinus2Point1XMinus2Point1()
+        {
+            Check.That(_parser.CanParse("-2.1x-2.1")).Equals(true);
+            Check.That(_parser.Extract("-2.1x-2.1")).ContainsExactly(new[] { -2.1, -2.1 });
+        }
+
+        [TestMethod]
+        public void TestPlusX()
+        {
+            Check.That(_parser.CanParse("+x")).Equals(true);
+        }
+
+        [TestMethod]
+        public void TestMinusX()
+        {
+            Check.That(_parser.CanParse("-x")).Equals(true);
+            Check.That(_parser.Extract("-x")).ContainsExactly(new[] { -1.0, 0.0 });
+        }
+
+        [TestMethod]
+        public void TestXPlus1()
+        {
+            Check.That(_parser.CanParse("x+1")).Equals(true);
+        }
+
+        [TestMethod]
+        public void TestXMinus1()
+        {
+            Check.That(_parser.CanParse("x-1")).Equals(true);
+        }
+
+        [TestMethod]
+        public void Test2XPlus1()
+        {
+            Check.That(_parser.CanParse("2x+1")).Equals(true);
+        }
+
+        [TestMethod]
+        public void TestXaab()
+        {
+            Check.That(_parser.CanParse("xaab")).Equals(false);
+        }
+
+        [TestMethod]
+        public void TestPlusMinusX()
+        {
+            Check.That(_parser.CanParse("+-x")).Equals(false);
+        }
+
+        [TestMethod]
+        public void Test2Point1XPlus2PointPlus()
+        {
+            Check.That(_parser.CanParse("2.1x+2.1+")).Equals(false);
+        }
+    }
+}
